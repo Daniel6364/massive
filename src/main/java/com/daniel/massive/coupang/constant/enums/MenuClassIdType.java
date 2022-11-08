80cf0d87a8077bcd729fcce066c2fdb10d74af15
iamsung33 <iamsung33@naver.com>
2022-11-08 18:09:49 +0900

    MenuClassIdType

---
+++ src/main/java/com/daniel/massive/coupang/constant/enums/MenuClassIdType.java
@@ -0,0 +1,22 @@
+package com.daniel.massive.coupang.constant.enums;
+
+public enum MenuClassIdType {
+
+    fashionsundries,
+    beauty,
+    childbirth,
+    food,
+    kitchen,
+    life,
+    homedecoration,
+    appliancesdigital,
+    sports,
+    car,
+    book,
+    hobby,
+    office,
+    pet,
+    health
+
+
+}
